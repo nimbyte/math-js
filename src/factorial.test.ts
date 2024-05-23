8c8054aec4132334c65f759b6cfeedc412bf4c29
Juan Pablo Rinaldi <jprinaldi@proton.me>
2024-05-23 11:22:34 -0300

    Improve factorial function

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -1,14 +1,38 @@
 import { expect, test } from "bun:test";
 import { factorial } from "./factorial";
 
-test("factorial(0)", () => {
+test("factorial(-1) throws an error", () => {
+  expect(() => factorial(-1)).toThrow();
+});
+
+test("factorial(0.1) throws an error", () => {
+  expect(() => factorial(0.1)).toThrow();
+});
+
+test("factorial(0) throws an error", () => {
+  expect(factorial(0)).toBe(1n);
+});
+
+test("factorial(1) throws an error", () => {
+  expect(factorial(1)).toBe(1n);
+});
+
+test("factorial(5) throws an error", () => {
+  expect(factorial(5)).toBe(120n);
+});
+
+test("factorial(-1n) throws an error", () => {
+  expect(() => factorial(-1n)).toThrow();
+});
+
+test("factorial(0n)", () => {
   expect(factorial(0n)).toBe(1n);
 });
 
-test("factorial(1)", () => {
+test("factorial(1n)", () => {
   expect(factorial(1n)).toBe(1n);
 });
 
-test("factorial(5)", () => {
+test("factorial(5n)", () => {
   expect(factorial(5n)).toBe(120n);
 });
--- src/factorial.ts
+++ src/factorial.ts
@@ -1,5 +1,20 @@
 /** This function computes the factorial of the given integer */
-export function factorial(n: bigint): bigint {
+export function factorial(n: number | bigint): bigint {
+  if (typeof n === "number") {
+    if (!Number.isInteger(n)) {
+      throw new RangeError("Factorial of non-integer numbers is not defined.");
+    }
+    return factorialBigint(BigInt(n));
+  } else {
+    return factorialBigint(n);
+  }
+}
+
+function factorialBigint(n: bigint): bigint {
+  if (n < 0) {
+    throw new RangeError("Factorial of negative numbers is not defined.");
+  }
+
   let result = 1n;
 
   while (n > 0) {
